6c189864b179019ac84a21930dfb4520c867e6ef
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2021-11-09 20:17:34 -0500

    filepath corrections to demo inference backend

--- Demo/demo_inference_backend.py
+++ Demo/demo_inference_backend.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 import numpy as np
-from Model_Prototypes import binary_cnn_with_embeddings
+from demo_model_builder import binary_cnn_with_embeddings
 from Pipeline.Training_Data_Pipeline import standardize_text, text_vectorizer
 
 
@@ -20,7 +20,7 @@ class backend_model:
 
     def single_prediction(self, input_text):
         text = standardize_text(input_text)
-        text_vector = text_vectorizer.apply(text)
+        text_vector = text_vectorizer(text)
         text_vector = np.array([text_vector,])
         prediction = {name:self.models[name].predict(text_vector)[0][0] for name in self.models.keys()}
 
@@ -28,9 +28,9 @@ class backend_model:
 
 
 model = backend_model()
-third_party_path = os.path.normpath('Prototype_Model_Weights/third_party_model/third_party_model')
-location_path = os.path.normpath('Prototype_Model_Weights/location_model/location_model')
-identifier_path = os.path.normpath('Prototype_Model_Weights/identifier_model/Best_Identifier_Model')
+third_party_path = os.path.normpath('Demo_Model_Weights/third_party_model/third_party_model')
+location_path = os.path.normpath('Demo_Model_Weights/location_model/location_model')
+identifier_path = os.path.normpath('Demo_Model_Weights/identifier_model/Best_Identifier_Model')
 
 model.add_model('IDENTIFIERS', identifier_path)
 model.add_model('LOCATION', location_path)
