bd4fd179cb7ecd7993153a71bc73ce8e3472d7a9
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2021-11-09 22:11:23 -0500

    demo filepath corrections

--- Demo/demo_model_builder.py
+++ Demo/demo_model_builder.py
@@ -29,7 +29,7 @@ def binary_cnn_with_embeddings(sequence_length, vocab_size, embedding_dim,
 
 
 
-data = pd.read_csv(os.path.normpath('Labeled_Data.csv'))
+data = pd.read_csv(os.path.normpath('Demo_Data.csv.csv'))
 x = data.segment_text
 y = data[[
     'IDENTIFIER',
