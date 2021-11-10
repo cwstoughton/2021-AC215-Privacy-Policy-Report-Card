f74779b99b6906250311c685bf431e9387c8d576
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2021-11-09 21:56:09 -0500

    Added line breaks back for test

--- Demo/docker-entrypoint.sh
+++ Demo/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Container is running!!!"
+
 # this will run the api/service.py file with the instantiated app FastAPI
-uvicorn_server() {uvicorn app:app --host 0.0.0.0 --port 9000 --log-level debug --reload --reload-dir / "$@"}
+uvicorn_server() {
+    uvicorn app:app --host 0.0.0.0 --port 9000 --log-level debug --reload --reload-dir / "$@"
+}
 
 uvicorn_server_production() {
     pipenv run uvicorn app:app --host 0.0.0.0 --port 9000 --lifespan on
