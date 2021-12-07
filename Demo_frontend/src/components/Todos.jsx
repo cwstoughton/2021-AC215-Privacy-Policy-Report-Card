c619c3e38c89db8155cb8a154608254cccad7dba
mathmind <mathmind@protonmail.com>
2021-12-07 14:24:50 +0900

    working gauge charts

--- Demo_frontend/src/components/Todos.jsx
+++ Demo_frontend/src/components/Todos.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {
     Box,
-    Button,
+    Button, Divider,
     Flex,
     Input,
     InputGroup,
@@ -16,6 +16,8 @@ import {
     Text,
     useDisclosure
 } from "@chakra-ui/core";
+import GaugeChart from 'react-gauge-chart';
+
 
 const TodosContext = React.createContext({
   todos: [], fetchTodos: () => {}
@@ -23,20 +25,40 @@ const TodosContext = React.createContext({
 
 export default function Todos() {
   const [todos, setTodos] = useState([])
+
+
+
   const fetchTodos = async () => {
-    const response = await fetch("http://localhost:9000/predict")
-    const todos = await response.json()
-    setTodos(todos.data)
+    const response = await fetch("http://34.134.205.218.sslip.io/api/predict")
+    const newTodos = await response.json()
+    // output the value of todos to console
+    console.log(newTodos)
+
+    setTodos(newTodos.data)
   }
-  console.log(todos.toString())
+  const listItems = todos.map((todo) =>
+                <GaugeChart id={todo["input_text"].toString()} style={{height:250, width:'10%', textColor:"#464A4F"}}  nrOfLevels={20}  percent={todo["predictions"]['IDENTIFIERS']} />
+  );
+
   useEffect(() => {
     fetchTodos()
   }, [])
   return (
     <TodosContext.Provider value={{todos, fetchTodos}}>
       <AddTodo />  {/* new */}
-      <Stack spacing={5}>
 
+      <Stack spacing={5} divider={<Divider orientation="vertical" flexItem />}>
+
+        {todos.map(todo => (
+            [<b>{todo.input_text}</b>, <br />,
+                <Stack direction="row">
+            [<GaugeChart id={todo["input_text"].toString()} style={{height:250, width:'10%', textColor:"#464A4F"}}  nrOfLevels={20}  percent={todo["predictions"]['IDENTIFIERS']} />,
+            <GaugeChart id={todo["input_text"].toString()} style={{height:250, width:'10%', textColor:"#464A4F"}}  nrOfLevels={20}  percent={todo["predictions"]['LOCATION']} />,
+            <GaugeChart id={todo["input_text"].toString()} style={{height:250, width:'10%', textColor:"#464A4F"}}  nrOfLevels={20}  percent={todo["predictions"]['3RD_PARTY']} />]
+                </Stack>]
+        ))}
+        ))}
+        ))}
       </Stack>
     </TodosContext.Provider>
   )
@@ -56,7 +78,7 @@ function AddTodo() {
       "item": item
     }
 
-        fetch(`http://localhost:9000/predict_new?input_text=${encodeURIComponent(newTodo.item)}`, {
+        fetch(`http://34.134.205.218.sslip.io/api/predict_new?input_text=${encodeURIComponent(newTodo.item)}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       // body: newTodo.item
@@ -78,3 +100,5 @@ function AddTodo() {
     </form>
   )
 }
+
+
