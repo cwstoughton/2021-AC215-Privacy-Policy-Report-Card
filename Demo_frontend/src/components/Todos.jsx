6c5f37e7dd52338f40d924bccdcd3fbcf1f48fba
Chris Lee <enerideas.chris@gmail.com>
2021-12-11 21:19:59 +0900

    working version with correct API

--- Demo_frontend/src/components/Todos.jsx
+++ Demo_frontend/src/components/Todos.jsx
@@ -1,46 +1,61 @@
 import React, {createContext, useEffect, useState} from "react";
-import {
-    Input,
-    InputGroup,
-    Stack
-} from "@chakra-ui/core";
-
+import {Text, Box, Input, InputGroup, Stack} from "@chakra-ui/core";
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import GaugeChart from 'react-gauge-chart';
+// import TabPanelMaker from './TabPanelMaker';
+import 'react-tabs/style/react-tabs.css';
 
 
 const APIContext = createContext({
-  data: {}, })
+    data: {},
+})
 
 export default function Todos() {
-    const [data, setData] = useState({input_text: "REACT initialize", predictions: {IDENTIFIERS: [], LOCATION:[], THIRD_PARTY:[]}})
-    const [policy_url, set_url] = React.useState("initiate")
+    const [data, setData] = useState({
+        input_text: "REACT initialize",
+        identifier_score: 0.3,
+        identifier_sentences: [],
+        location_score: 0.5,
+        location_sentences: [],
+        third_party_score: 0.8,
+        third_party_sentences: []
+    })
+    const [locationItems, setLocation] = useState()
+    const [trackerItems, setIdentifiers] = useState()
+    const [sharingItems, setThirdParty] = useState()
 
-        console.log(policy_url)
+
+    const [policy_url, set_url] = React.useState("")
+
+    console.log(policy_url)
 
 
-  useEffect(() => {
-      const callApi = async () => {
-        const response = await fetch('http://localhost:9000/analyze', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
+    useEffect(() => {
+        const callApi = async () => {
+            const response = await fetch('http://localhost:9000/analyze', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
                 mode: 'cors',
-            body: JSON.stringify({input : policy_url})})
-          const result = response.json()
-          return result
-      }
-      callApi().then(data => {
-          console.log(data.data)
-          setData(data.data)})}, [policy_url])
+                body: JSON.stringify({input: policy_url})
+            })
+            return response.json()
+        }
+        callApi().then(data => {
+            console.log(data.data)
+            setData(data.data)
+        })
+    }, [policy_url])
 
     function AnalyzePolicy() {
 
         var placeholderUrl = ""
-        const handleInput = event  => {
+        const handleInput = event => {
             placeholderUrl = event.target.value
         }
-        const handleSubmit = (event) =>  {
+        const handleSubmit = (event) => {
             set_url(placeholderUrl)
         }
         return (
@@ -59,14 +74,129 @@ export default function Todos() {
     }
 
 
-  return (
-      <div>
-          <APIContext.Provider value={{data}}>
-              <AnalyzePolicy />
-              <Stack spacing={1}>
-                  [<b>{data.input_text}</b>]+{data.predictions.IDENTIFIERS.map(item => (<b>{item}</b>))}
-              </Stack>
-          </APIContext.Provider>
-      </div>
-  )
+    const chartStyle = {width: "33%", formatTextValue: (value => (value > 0.5)), textColor: {color: "FF00FF"}}
+    const Component = (
+
+
+        <Stack spacing={50}>
+
+            [
+            <Tabs><TabList>
+                <Tab>Overview</Tab>
+                <Tab>Identifiers</Tab>
+                <Tab>Location Data</Tab>
+                <Tab>3rd Party Sharing</Tab>
+            </TabList>
+                {/*pass data.identifier_score and data.identifier_sentences to TabPanelMaker*/}
+
+                <TabPanel>
+                    <Box style={{backgroundColor: "#FFFFFF"}}>
+                        <h2 style={{color: "white"}}>Overview</h2>
+                        <Text><font color="#00FF00">INPUT TEXT:</font> {data.input_text}</Text>
+                        <Stack direction="row">
+                            [
+                            <Stack>[<Text
+                                style={{
+                                    textAlignVertical: "center",
+                                    textAlign: "center",
+                                }}>IDENTIFIERS</Text>,<GaugeChart
+                                id={data.input_text.toString() + "_ID"} style={{width: "100%"}} nrOfLevels={20}
+                                textColor="#000000" colors={["#FFC371", "#FF5F6D"]}
+                                percent={data.identifier_score}/>]</Stack>,
+                            <Stack>[<Text
+                                style={{textAlignVertical: "center", textAlign: "center",}}>LOCATION</Text>,<GaugeChart
+                                id={data.input_text.toString() + "_LC"} style={{width: "100%"}} textColor="#000000"
+                                nrOfLevels={20} colors={["#C3FF71", "#5FFF6D"]} percent={data.location_score}/>]</Stack>,
+                            <Stack>[<Text style={{textAlignVertical: "center", textAlign: "center",}}>3RD
+                                PARTY </Text>,<GaugeChart id={data.input_text.toString() + "_3D"}
+                                                          style={{width: "100%"}}
+                                                          textColor="#000000" nrOfLevels={20}
+                                                          colors={["#C371FF", "#5F6DFF"]}
+                                                          percent={data.third_party_score}/>]</Stack>
+                            ]
+                        </Stack>
+
+                    </Box>
+                </TabPanel>
+
+
+                <TabPanel>
+                    <Stack direction="row">
+                        [
+                        <Box style={{backgroundColor: "#FFFFFF"}}>
+                            <h2>Identifiers</h2>
+                            <GaugeChart id="1" style={{width: "100%"}} nrOfLevels={20} textColor="#000000"
+                                        colors={["#FFC371", "#FF5F6D"]} percent={data.identifier_score}/>
+                        </Box>,
+                        <Box>
+                            <ul>
+                                {data.identifier_sentences.map(item => <li>{item}</li>)}
+                            </ul>
+                        </Box>
+                        ]
+                    </Stack>
+                </TabPanel>
+                <TabPanel>
+                    <Stack direction="row">
+                        [
+                        <Box style={{backgroundColor: "#FFFFFF"}}>
+                            <h2>Location Data</h2>
+                            <GaugeChart id={data.input_text + "_LC"} style={{width: "100%"}}
+                                        textColor="#000000" nrOfLevels={20} colors={["#C3FF71", "#5FFF6D"]}
+                                        percent={data.location_score}/>
+                        </Box>,
+                        <Box>
+                            <ul>
+                                {data.location_sentences.map(item => <li>{item}</li>)}
+                            </ul>
+                        </Box>
+                        ]
+                    </Stack>
+                </TabPanel>
+                <TabPanel>
+                    <Stack direction="row">
+                        [
+                        <Box style={{backgroundColor: "#FFFFFF"}}>
+                            <h2>3rd Party Sharing</h2>
+                            <GaugeChart id={data.input_text + "_3D"} style={{width: "100%"}}
+                                        textColor="#000000" nrOfLevels={20} colors={["#C371FF", "#5F6DFF"]}
+                                        percent={data.third_party_score}/>
+                        </Box>,
+                        <Box>
+                            <ul>
+                                {data.third_party_sentences.map(item => <li>{item}</li>)}
+                            </ul>
+                        </Box>
+                        ]
+                    </Stack>
+                </TabPanel>
+            </Tabs>
+            ]
+            )}
+        </Stack>
+
+    );
+
+
+    return (<div>
+            <APIContext.Provider value={{data}}>
+
+                <AnalyzePolicy/> {/* new */}
+                {Component}
+            </APIContext.Provider></div>
+    )
 }
+//   return (
+//       <div>
+//           <APIContext.Provider value={{data}}>
+//               <AnalyzePolicy />
+//               <Stack spacing={1}>
+//
+//                   [<b>{data.input_text}: {data.identifier_score}</b>, ]+{data.identifier_sentences.map(item => (<b>{item}</b>))}
+//               </Stack>
+//           </APIContext.Provider>
+//       </div>
+//   )
+// }
+
+
