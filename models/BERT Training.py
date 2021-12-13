8bee6bba85cd0170bfcc190f2c8f4053c0c0b4b3
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2021-12-12 21:50:18 -0500

    File was ultimately not used directly. See notebooks for final model training scripts

--- models/BERT Training.py
+++ models/BERT Training.py
@@ -16,13 +16,17 @@ from tensorflow.keras.models import Model, Sequential
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.utils.layer_utils import count_params
 
-from transformers import BertTokenizer, TFBertForSequenceClassification, TFDistilBertForSequenceClassification, TFAutoModelForTokenClassification
+from transformers import BertTokenizer, TFBertForSequenceClassification,TFDistilBertForMaskedLM , TFDistilBertModel, TFDistilBertForSequenceClassification, TFAutoModelForTokenClassification, DistilBertConfig
 from transformers import TFTrainer, TrainingArguments
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
-from transformers import AutoTokenizer, DistilBertModel
+from transformers import AutoTokenizer, DistilBertModel, pipeline
+
+physical_devices = tf.config.list_physical_devices('GPU')
+
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 print("tensorflow version", tf.__version__)
@@ -43,7 +47,7 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 print('Setup checks complete...\n')
 
-print('Prepairing training data...')
+print('Preparing training data...')
 training_data_path = os.path.normpath('../Data/Labeled_Data.csv')
 data = pd.read_csv(training_data_path)
 
@@ -53,18 +57,107 @@ def standardize_text(text):
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
     return text
 
+def truncate_text(text, max_len):
+    return ' '.join(text.split()[:max_len])
+
+def pad(array, max_length):
+    return array + [0] * (max_length - len(array))
+
+max_len = 512
+
+tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+
 data['segment_text'] = data['segment_text'].apply(standardize_text)
 
 X = data['segment_text']
 y = data[['3RD', 'LOCATION', 'DEMOGRAPHIC', 'CONTACT', 'IDENTIFIER', 'SSO']].astype(int)
 
+#X = X.apply(lambda x: truncate_text(x, max_len))
+
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.25, random_state=17921)
 
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+X_train= X_train.apply(lambda x: tokenizer.encode(x, truncation=True, padding = True, max_length=max_len))
+X_test= X_test.apply(lambda x: tokenizer.encode(x, truncation=True, padding = True, max_length=max_len))
 
-X_train= tokenizer(list(X_train), truncation=True, padding = True)
-X_test= tokenizer(list(X_test), truncation=True, padding = True)
 
-BERT = TFDistilBertForSequenceClassification.from_pretrained(r'models/BERT_BASE')
 
-control = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+
+X_train = X_train.apply(lambda x: pad(x,max_len))
+X_test = X_test.apply(lambda x: pad(x,max_len))
+
+X_train = list(X_train)
+X_test = list(X_test)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+
+
+config = DistilBertConfig(dropout=.2,
+                          attention_dropout=.2,
+                          output_hidden_states=True)
+
+BERT = TFDistilBertModel.from_pretrained(r'models/BERT_BASE_2', config = config)
+
+for layer in BERT.layers:
+    layer.trainable = False
+
+
+loss = tf.keras.losses.binary_crossentropy
+
+
+def get_embeddings(transformer, max_length = 512, cls_only = False):
+    input_ids_layer = keras.layers.Input(shape=(max_length,),
+                                         name='input_ids',
+                                         dtype='int32')
+    embeddings = transformer([input_ids_layer])[0]
+
+    if cls_only == True:
+        embeddings = transformer([input_ids_layer])[0]
+
+
+
+def build_component_model(transformer, max_length=512):
+    input_layer = keras.layers.Input(shape=(max_length,),
+                                            name='input_ids',
+                                            dtype='int32')
+    embeddings = transformer([input_layer])[0]
+
+    #cls_token = embeddings[:, 0, :]
+    hidden = keras.layers.Conv1D(filters=32, kernel_size=2, padding="valid", activation="relu")(embeddings)
+    hidden = keras.layers.GlobalMaxPooling1D()(hidden)
+    hidden = keras.layers.Dense(units=128, activation="tanh")(hidden)
+    hidden = keras.layers.Dense(units=64, activation="tanh")(hidden)
+    hidden = keras.layers.Dense(units=32, activation="tanh")(hidden)
+
+
+
+    # Define a single node that makes up the output layer (for binary classification)
+    output = keras.layers.Dense(1,activation='sigmoid')(hidden)
+
+
+    model = tf.keras.Model([input_layer], output)
+
+    # Compile the model
+    model.compile(optimizer = "Adam", loss=loss,  metrics = 'binary_accuracy')
+
+    return model
+
+
+
+def binary_classification_head(max_length):
+    input_ids_layer = keras.layers.Input(shape=(max_length,),
+                                         name='input_ids',
+                                         dtype='int32')
+    hidden = keras.layers.Conv1D(filters=32, kernel_size=2, padding="valid", activation="relu")(embeddings)
+    hidden = keras.layers.GlobalMaxPooling1D()(hidden)
+    hidden = keras.layers.Dense(units=128, activation="tanh")(hidden)
+    hidden = keras.layers.Dense(units=64, activation="tanh")(hidden)
+    hidden = keras.layers.Dense(units=32, activation="tanh")(hidden)
+    # Define a single node that makes up the output layer (for binary classification)
+    output = keras.layers.Dense(1, activation='sigmoid')(hidden)
+    model = tf.keras.Model([input_ids_layer], output)
+    # Compile the model
+    model.compile(optimizer="Adam", loss=loss, metrics='binary_accuracy')
+    return model
+
+
